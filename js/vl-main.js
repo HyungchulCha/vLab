b10c8e2bb9953c820e54abb677df5099b3dc6553
차형철 <ckgudcjf@chahyeongcheol-ui-MacBookPro.local>
2022-05-07 16:52:42 +0900

    main complete

--- js/vl-main.js
+++ js/vl-main.js
@@ -11,8 +11,8 @@ function mScroll() {
     var win = $(window),
         hd = $('.header');
 
-    win.scroll(function () {
-        if (win.scrollTop() > mWinH) {
+    win.stop().scroll(function () {
+        if (win.scrollTop() >= (mWinH - ($('.header').outerHeight()))) {
             hd.addClass('hd_w');
         } else {
             hd.removeClass('hd_w');
@@ -23,17 +23,31 @@ function mScroll() {
 
 /* main - visual height check */
 
-function mVisual() {
+function mVisualHeight() {
     $(this).css('height', $(window).outerHeight());
+}
+function mVisualScroll() {
+    $('.m_visual').stop().on('mousewheel', function (e) {
+        e.deltaY === -1 && $('html').animate({ scrollTop: (mWinH - ($('.header').outerHeight())) }, 480, 'easeOutCubic');
+    });
+}
+function mVisualDown() {
+    $('.m_visual .mv_scroll').on('click', function () {
+        $('html').animate({ scrollTop: (mWinH - ($('.header').outerHeight())) }, 400, 'easeOutCubic');
+        return false;
+    });
 }
 
 $(document).ready(function () {
 
     afterHasCheck('.main', mWinHeight, true);
     mScroll();
-    afterHasCheck('.m_visual', mVisual, true);
-    fnSlide({ dom: '.m_pick > div', loop: true, auto: false, center: false});
-    fnSlide({ dom: '.m_news > div', loop: true, auto: false, center: true, breakPoint: ['p', 't']});
-    
+    afterHasCheck('.m_visual', mVisualHeight, true);
+    mVisualScroll();
+    mVisualDown();
+    fnSlide({ dom: '.m_pick .mp_slide', loop: false, auto: false, center: false });
+    afterHasCheck('.m_pick .mp_img', domRatio, true, (3 / 4))
+    fnSlide({ dom: '.m_news .mn_slide', loop: true, auto: false, center: true, breakPoint: ['p', 't'] });
+
 });
 
