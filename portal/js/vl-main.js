ca69592ab83838b5593bf6e59551fd699a39ef4a
차형철 <ckgudcjf@ckgudcjf-MacBookPro.local>
2022-06-14 21:58:21 +0900

    end

--- portal/js/vl-main.js
+++ portal/js/vl-main.js
@@ -1,54 +1,48 @@
-/* scroll + header */
-
-var mWinH = 0;
-
-function mWinHeight() {
-    mWinH = $(window).outerHeight();
-}
-
-function mScroll() {
-
-    var win = $(window),
-        hd = $('.header');
-
-    win.scroll(function () {
-        if (win.scrollTop() >= (mWinH - ($('.header').outerHeight()))) {
-            hd.addClass('hd_w');
-        } else {
-            hd.removeClass('hd_w');
-        }
-    });
-
-}
-
 /* main - visual height check */
 
 function mVisualHeight() {
     $(this).css('height', $(window).outerHeight());
 }
-function mVisualScroll() {
-    $('.m_visual').on('mousewheel', function (e) {
-        e.deltaY === -1 && $('html').animate({ scrollTop: (mWinH - ($('.header').outerHeight())) }, 480, 'easeOutCubic');
-    });
-}
+
 function mVisualDown() {
     $('.m_visual .mv_scroll').on('click', function () {
-        $('html').animate({ scrollTop: (mWinH - ($('.header').outerHeight())) }, 400, 'easeOutCubic');
+        $('html').stop().animate({ scrollTop: ($(window).outerHeight() - ($('.header').outerHeight())) }, 400, 'easeOutCubic');
         return false;
     });
 }
 
 $(document).ready(function () {
 
-    afterHasCheck('.main', mWinHeight, true);
-    mScroll();
     afterHasCheck('.m_visual', mVisualHeight, true);
     fnSlide({ dom: '.m_pick .mp_slide', loop: false, auto: false, center: false });
     afterHasCheck('.m_pick .mp_img', domRatio, true, (3 / 4));
     fnSlide({ dom: '.m_news .mn_slide', loop: true, auto: false, center: true, breakPoint: ['p', 't'] });
-
-    mVisualScroll();
     mVisualDown();
 
 });
+
+var listen = true;
+
+$('html').on('mousewheel DOMMouseScroll', function (e, delta) {
+    var offTop = $(window).outerHeight() - $('.header').outerHeight();
+    if (e.offsetY > offTop) return;
+    if (delta > 0) return;
+    if (!listen) return;
+    listen = false;
+    delta < 0 && $('html, body').animate({ scrollTop: offTop }, 1000, 'easeOutCubic', function(){
+        console.log('end');
+        listen = true;
+    });
+    
+});
+
+$(window).stop().on('scroll', $.throttle(160, function(e) {
+    var hd = $('.header'),
+        offTop = $(window).outerHeight() - hd.outerHeight();
+    if ($(window).scrollTop() >= offTop) {
+        hd.addClass('hd_w');
+    } else {
+        hd.removeClass('hd_w');
+    }
+}));
 
